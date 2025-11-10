65c15f8aef8e3f293abadade9b25113795c969d2
Emmanuel Oduntan <oduntane@icloud.com>
2025-11-10 11:17:48 +0100

    test: add tests for postService

---
+++ services/__tests__/postService.test.ts
@@ -0,0 +1,38 @@
+
+describe('postService', () => {
+    describe('uploadImage()', () => {
+        it('should call supabase storage upload with correct file', () => {
+
+        })
+
+        it('should return public URL on success', () => {
+            
+        })
+
+        it('should throw error on upload failure', () => {
+
+        })
+    })
+
+    describe('createPost', () => {
+        it('should call supabase insert with correct data', () => {
+
+        })
+
+        it('should return created post object', () => {
+
+        })
+
+        it('should throw error if userId is missing', () => {
+
+        })
+
+        it('should throw error if userId is missing', () => {
+
+        })
+
+        it('should throw error if text is empty', () => {
+            
+        })
+    })
+})
